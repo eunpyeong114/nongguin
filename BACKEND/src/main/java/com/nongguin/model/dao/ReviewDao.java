8e6860d1b3892e4be0e9c55e474c0ce6bbbf4c32
eunpyeong114 <eunpyeongjeon@gmail.com>
2023-12-04 12:18:22 +0900

    ADD:Review Dao

--- BACKEND/src/main/java/com/nongguin/model/dao/ReviewDao.java
+++ BACKEND/src/main/java/com/nongguin/model/dao/ReviewDao.java
@@ -1,5 +1,7 @@
 package com.nongguin.model.dao;
 
+import java.util.List;
+
 import com.nongguin.model.dto.Review;
 
 public interface ReviewDao {
@@ -7,5 +9,11 @@ public interface ReviewDao {
 	public int registReview(Review review);
 
 	public int deleteReview(Review review);
+
+	public int modifyReview(Review review);
+
+	public List<Review> getReviewsByCourtId(int courtId);
+
+	public Review getReviewByPaymentId(int paymentId);
 
 }
