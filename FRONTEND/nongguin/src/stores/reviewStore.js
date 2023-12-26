c75e0363c9fbfabaf79161749dfcfdb476f9486a
eunpyeong114 <eunpyeongjeon@gmail.com>
2023-12-26 18:28:19 +0900

    ADD: review store

---
+++ FRONTEND/nongguin/src/stores/reviewStore.js
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+// 리뷰 스토어
+export const useReviewStore = defineStore("review", () => {
+  // 리뷰 등록
+  const registReview = function () {};
+  // 리뷰 수정
+
+  // 리뷰 삭제
+
+  // 농구장별 리뷰 전체 조회하기
+
+  // 매치 결제별  리뷰 조회하기
+});
