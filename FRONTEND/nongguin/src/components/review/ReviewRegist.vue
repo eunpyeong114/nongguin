59ece7d0c3e5bb92374974874ddd1a1045e9c19e
eunpyeong114 <eunpyeongjeon@gmail.com>
2023-12-26 18:27:50 +0900

    ADD: review component 생성

---
+++ FRONTEND/nongguin/src/components/review/ReviewRegist.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span id="reviewTitle">리뷰 작성</span>
+  </div>
+  <div class="form-floating">
+    <textarea
+      class="form-control"
+      placeholder="리뷰를 작성해주세요"
+      id="floatingTextarea"
+    ></textarea>
+    <label for="floatingTextarea">Comments</label>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
